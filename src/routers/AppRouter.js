d0140893082161f1c0377cb7b9553990391e85fa
Debashish Nayak <debashishzero@gmail.com>
2018-02-24 06:36:08 +0530

    Preparation to deploy to heroku

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
 import App from '../components/App';
 import Header from '../components/Common/Header';
 
 export const history = createHistory();
+
+const RouteNotFound = () => <Redirect to="/" />;
 
 const AppRouter = () => (
   <Router history={history} >
@@ -14,6 +16,7 @@ const AppRouter = () => (
       <main className='main-container ui container'>
         <Switch>
           <Route path="/" component={App} exact={true} />
+          <RouteNotFound />
         </Switch>
       </main>
     </div>
